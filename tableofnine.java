905df62f3b1579ab000c716a01f821cc593832f5
Nainajivanji <167822064+Nainajivanji@users.noreply.github.com>
2024-05-16 18:33:40 +0100

    Using while loop
    Assignment:
    5. Write a program to generate tables of 9.

---
+++ tableofnine.java
@@ -0,0 +1,21 @@
+package whileloop;
+
+public class tableofnine {
+    public static void main(String[] args) {
+        int N = 9;
+        int j = 1;
+        do {
+            System.out.println(N + "*" + j + "=" + N*j);
+                j++;
+            }
+            while (j <= 12) ;
+
+
+        }
+    }
+
+
+
+
+
+
